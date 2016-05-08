e3bf526bdf8607c3c189be258949ec6f882cc7ff
Tyler Bettilyon <teb311@gmail.com>
2016-05-07 18:28:57 -0600

    lots of working input stuff, boosh

--- satView.js
+++ satView.js
@@ -5,11 +5,6 @@ const LS_API_SECRET = 'uc_api_secret';
 const API_KEY = getOrPrompt(LS_API_KEY);
 const API_SECRET = getOrPrompt(LS_API_SECRET);
 
-// SF's location information
-const SF_LAT = 37.78684346730307;
-const SF_LONG = -122.40559101104735;
-const DEFAULT_ZOOM = 9;
-
 // menu-ui togglers
 const ACTIVE_CLASS = "active";
 const INACTIVE_CLASS = "";
@@ -18,62 +13,112 @@ const LAYERS = ['rgb', 'ndvi', 'ndwi', 'false-color-nir', 'evi'];
 const SENSORS = ['theia', 'landsat-8', 'deimos-1'];
 const SEASONS = ['summer', 'fall', 'winter', 'spring'];
 
-const FILTERS = {
-  sensor_platform: SENSORS.join(','),
-  season: SEASONS.join(','),
-  cloud_coverage_lte: 100,
-  sun_elevation_lte: 90
+const DEFAULT_OPTIONS = {
+  latitude: 37.78684346730307,
+  longitude: -122.40559101104735,
+  zoom: 9,
+  filters: {
+    sensor_platform: SENSORS.join(','),
+    season: SEASONS.join(','),
+    cloud_coverage_lte: 100,
+    sun_elevation_lte: 90
+  }
 };
 
+// Onload we grab and set default input fields, add a listener for our button, and initialize
+var MAP_GLOBAL;
+window.onload = function(event){
+  // Allow menu hiding
+  document.getElementById("hide-menus").addEventListener('click', hideMenuHandler);
 
-window.onload = function() {
-  var map = L.map('map');
+  // Show starting lat/long
+  var latInput = document.getElementById('lat-in');
+  latInput.value = DEFAULT_OPTIONS.latitude;
 
-  // Lets look at SF
-  setMapLocation(map, SF_LAT, SF_LONG, DEFAULT_ZOOM);
+  var longInput = document.getElementById('long-in');
+  longInput.value = DEFAULT_OPTIONS.longitude;
 
-  // Create the layers
-  for(let i = 0; i < LAYERS.length; i++) {
-    let colorLayer = LAYERS[i];
-    let url = createTileUrl(colorLayer, FILTERS);
+  var cloudInput = document.getElementById('cloud-cover-in');
+  cloudInput.value = DEFAULT_OPTIONS.filters.cloud_coverage_lte;
 
-    // Allow the layers to be toggled. 
-    addLayerToggle(L.tileLayer(url), colorLayer, i, map);
-  }
+  var sunElevInput = document.getElementById('sun-elevation-in');
+  sunElevInput.value = DEFAULT_OPTIONS.filters.sun_elevation_lte;
+
+  // Listen for lat-long changes
+  var changeLocationButton = document.getElementById('new-location-btn');
+  changeLocationButton.addEventListener('click', resetMapOptions);
 
   // Add the sensor-toggle elements
   for(let i = 0; i < SENSORS.length; i++) {
     let sensorName = SENSORS[i];
     addSensorToggle(sensorName);
   }
+  
+  MAP_GLOBAL = L.map('map');
+  MAP_GLOBAL.setView(L.latLng(DEFAULT_OPTIONS.latitude, DEFAULT_OPTIONS.longitude), DEFAULT_OPTIONS.zoom);
 
-  // Allow menu hiding
-  document.getElementById("hide-menus").addEventListener('click', hideMenuHandler);
+  initializeMap();
+};
 
-  // Show starting lat/long
-  var latInput = document.getElementById('lat-in');
-  var longInput = document.getElementById('long-in');
-  latInput.value = SF_LAT;
-  longInput.value = SF_LONG;
+function initializeMap(options) {
+  map = MAP_GLOBAL;
+  if(options === undefined) {
+    options = DEFAULT_OPTIONS;
+  }
 
-  // Listen for lat-long changes
-  var changeLocationButton = document.getElementById('new-location-btn');
-  changeLocationButton.addEventListener('click', function(event) {
-    setMapLocation(map, latInput.value, longInput.value);
-  });
+  // Use defaults for unset properties
+  for(var key in DEFAULT_OPTIONS) {
+    if(!options[key]) {
+      options[key] = DEFAULT_OPTIONS[key];
+    }
+  }
+
+  map.panTo(L.latLng(options.latitude, options.longitude));
+
+  // Create the layers
+  var layerToggles = document.getElementById('layer-toggle');
+  layerToggles.innerHTML = '';
+
+  for(let i = 0; i < LAYERS.length; i++) {
+    let layerName = LAYERS[i];
+    let initiallyOn = i === 0;
+    addLayerToggle(layerName, i, map, initiallyOn, options.filters);
+  }
 }
 
-/**
- * Given a latitude, logitude, and zoom[0-15] values, move the map
- * wherever it needs to go.
- */
-function setMapLocation(map, latitude, longitude, zoom) {
-  var latLong = L.latLng(latitude, longitude);
-  map.setView(latLong, zoom);
+function resetMapOptions() {
+  var latInput = document.getElementById('lat-in');
+  var longInput = document.getElementById('long-in');
+  var cloudInput = document.getElementById('cloud-cover-in');
+  var sunElevInput = document.getElementById('sun-elevation-in');
+  var activeSensors = document.querySelectorAll(`#sensor-toggle .${ACTIVE_CLASS}`);
+  
+  var sensors = [];
+  for(let i = 0; i < activeSensors.length; i++) {
+    sensors.push(activeSensors[i].innerText);
+  }
+
+  var options = {
+    latitude: latInput.value,
+    longitude: longInput.value,
+    zoom: DEFAULT_OPTIONS.zoom,
+    filters: {
+      sensor_platform: sensors.join(','),
+      season: SEASONS.join(','),
+      cloud_coverage_lte: cloudInput.value,
+      sun_elevation_lte: sunElevInput.value
+    }
+  }
+
+  map.eachLayer(function(layer) {
+    map.removeLayer(layer); 
+  });
+
+  initializeMap(options);
 }
 
 /* * 
- * Create a tile layer url based on filters and the color-set desired
+ * Create a tile layer url based on options and the color-set desired
  *  @param colorLayer: UrtheCast API values: 'rgb', 'ndvi', 'ndwi', 'false-color-nir', 'evi'
  *  @param filterValues: UrtheCast API key/value pairs for query params
  */
@@ -95,7 +140,7 @@ function createTileUrl(colorLayer, filterValues) {
 function hideMenuHandler(event) {
   event.preventDefault();
   event.stopPropagation();
-  
+
   var menus = document.querySelectorAll('.menu-ui.hideable');
   var targetStyle;
 
@@ -123,7 +168,7 @@ function hideMenuHandler(event) {
 function addSensorToggle(name) {
   var link = document.createElement('a');
       link.href = '#';
-      link.className = INACTIVE_CLASS;
+      link.className = ACTIVE_CLASS;
       link.innerHTML = name;
 
   link.onclick = function(event) {
@@ -153,7 +198,10 @@ function addSensorToggle(name) {
  * @param zIndex: a number, the zIndex of the layer on the map
  * @param map: a reference to the map object which will hold the layers
  */
-function addLayerToggle(layer, name, zIndex, map) {
+function addLayerToggle(name, zIndex, map, initOn, filters) {
+  let url = createTileUrl(name, filters);
+  var layer = L.tileLayer(url);
+
   layer.setZIndex(zIndex);
 
   // Create a simple layer switcher that
@@ -162,6 +210,11 @@ function addLayerToggle(layer, name, zIndex, map) {
       link.href = '#';
       link.className = INACTIVE_CLASS;
       link.innerHTML = name;
+
+  if(initOn) {
+    link.className = ACTIVE_CLASS;
+    map.addLayer(layer);
+  }
 
   link.onclick = function(event) {
     event.preventDefault();
