3bdaf3e6baf7a8f8e70355e9d3b2b54cc8644fea
Tyler Bettilyon <teb311@gmail.com>
2016-05-14 16:58:39 -0700

    added simple cloud filter

--- js/components/urtheCastMenu.js
+++ js/components/urtheCastMenu.js
@@ -1,8 +1,4 @@
 "use strict"
-const _CONFIG = {
-  hiddenClass: 'hidden',
-}
-
 /*
   A MapMenu is an overlay with options for controlling the maps behavior.
   This might be anything from toggling map layers on and off, or selecting
@@ -37,8 +33,18 @@ class MapMenuProto extends HTMLElement {
         '<input type="submit" value="Go">' +
       '</form>';
     this.latLongSection.innerHTML = latLongHTML;
-
     this.latLongSection.addEventListener('submit', this.handleLatLongSubmit.bind(this));
+
+    // TODO: Create a two-sided slider component that can be used for this and sun-angle.
+    this.cloudCoverageSection = new MenuSection();
+    var latLongHTML = '' +
+      '<form>' +
+        '<input type="number" name="cloud-coverage" placeholder="cloud-coverage" step="any">' +
+        '<input type="submit" value="Go">' +
+      '</form>';
+    this.cloudCoverageSection.innerHTML = latLongHTML;
+    this.cloudCoverageSection.addEventListener('submit', this.handleCloudCoverageSubmit.bind(this));
+
 
     // Create the sensor toggles
     for(let i = 0; i < UrtheCastMapProto.sensors.length; i++) {
@@ -67,13 +73,25 @@ class MapMenuProto extends HTMLElement {
     this.appendChild(this.layerSection);
     this.appendChild(this.sensorSection);
     this.appendChild(this.latLongSection);
+    this.appendChild(this.cloudCoverageSection);
   }
 
+  /*
+    Pans the map to the current lat/long input values
+  */
   handleLatLongSubmit(event) {
     event.preventDefault();
     var lat = Number(event.currentTarget.querySelector('input[name="latitude"]').value);
     var long = Number(event.currentTarget.querySelector('input[name="longitude"]').value);
     this.map.panTo(new L.LatLng(lat, long));
+  }
+
+  /*
+    Reset the layer URL's to filter based on the current cloud coverage input value
+  */
+  handleCloudCoverageSubmit(event) {
+    event.preventDefault();
+    this.updateTileUrls();
   }
 
   /*
@@ -106,6 +124,13 @@ class MapMenuProto extends HTMLElement {
 
     // sensor_platform is an UrtheCast value
     filters.sensor_platform = activeSensors.join(',');
+
+    // Add cloud coverage filter
+    var cloudCoverageLTE = parseFloat(this.cloudCoverageSection.querySelector('input[name=cloud-coverage]').value);
+    if(typeof cloudCoverageLTE !== 'number' || isNaN(cloudCoverageLTE)) {
+      cloudCoverageLTE = 100; // Most permisve value
+    }
+    filters.cloud_coverage_lte = cloudCoverageLTE;
 
     // Give each layer a new url based on our known filter situation
     for(let i = 0; i < this.layerSection.children.length; i++) {
